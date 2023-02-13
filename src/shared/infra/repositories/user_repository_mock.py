260256e19eaccc95da4d22af1a279789c4ad1a83
Luigi Trevisan <checkzin123@gmail.com>
2023-02-12 21:50:38 -0300

    métodos removidos do repo

--- src/shared/infra/repositories/user_repository_mock.py
+++ src/shared/infra/repositories/user_repository_mock.py
@@ -17,35 +17,3 @@ class UserRepositoryMock(IUserRepository):
             User(name="João Vilas", email="bruno@bruno.com", user_id=3, state=STATE.PENDING)
         ]
         self.user_counter = 3
-
-    def get_user(self, user_id: int) -> User:
-        for user in self.users:
-            if user.user_id == user_id:
-                return user
-        raise NoItemsFound("user_id")
-
-    def get_all_user(self) -> List[User]:
-        return self.users
-
-    def create_user(self, new_user: User) -> User:
-        self.users.append(new_user)
-        self.user_counter += 1
-        return new_user
-
-    def delete_user(self, user_id: int) -> User:
-        for idx, user in enumerate(self.users):
-            if user.user_id == user_id:
-                return self.users.pop(idx)
-
-        raise NoItemsFound("user_id")
-
-    def update_user(self, user_id: int, new_name: str) -> User:
-        for user in self.users:
-            if user.user_id == user_id:
-                user.name = new_name
-                return user
-
-        raise NoItemsFound("user_id")
-
-    def get_user_counter(self) -> int:
-        return self.user_counter
